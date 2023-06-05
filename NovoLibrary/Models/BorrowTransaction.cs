e7bc7221f9111a8212445338221c923799fb1d54
Jensbol <mjj.johansen@outlook.com>
2023-06-05 19:41:04 +0200

    feat: Add library Models

---
+++ NovoLibrary/Models/BorrowTransaction.cs
@@ -0,0 +1,14 @@
+﻿namespace NovoLibrary.Models
+{
+    public class BorrowTransaction
+    {
+        public int Id { get; set; }
+
+        public int BookId { get; set; }
+        public int MemberId { get; set; }
+        
+        public DateTime BorrowDate { get; set; }
+
+        public DateTime? ReturnDate { get; set; }
+    }
+}
