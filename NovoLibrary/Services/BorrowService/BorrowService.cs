1915ab43d15691f95d9d45e845f8db779a3151ed
Jensbol <mjj.johansen@outlook.com>
2023-06-05 22:10:34 +0200

    feat: Add BorrowService for GetBooksByMember, BorrowBook, ReturnBook

---
+++ NovoLibrary/Services/BorrowService/BorrowService.cs
@@ -0,0 +1,59 @@
+﻿using NovoLibrary.Core;
+using NovoLibrary.Models;
+
+namespace NovoLibrary.Services.BorrowService
+{
+    public class BorrowService : IBorrowService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public BorrowService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<List<BorrowTransaction>> BorrowBook(BorrowTransaction borrowTransaction)
+        {
+            var book = await _unitOfWork.BookRepository.GetById(borrowTransaction.BookId);
+            
+            if (book is null)
+                return null;
+
+            if (!book.IsAvailable)
+                throw new System.InvalidOperationException("Book is not available");
+
+            book.IsAvailable = false;
+
+            await _unitOfWork.BookRepository.Update(book);
+            await _unitOfWork.BorrowTransactionRepository.Add(borrowTransaction);
+            await _unitOfWork.CompleteAsync();
+
+            return await _unitOfWork.BorrowTransactionRepository.GetAll();
+        }
+
+        public async Task<List<Book>> GetBooksByMember(int memberId)
+        {
+            var booksByMember = await _unitOfWork.BorrowTransactionRepository.getBooksByMember(memberId);
+            return booksByMember.FindAll(b => b.IsAvailable == false);
+        }
+
+        public async Task<List<BorrowTransaction>> ReturnBook(int borrowId)
+        {
+            var bookToReturn = await _unitOfWork.BorrowTransactionRepository.GetById(borrowId);
+            if (bookToReturn is null)
+                return null;
+
+            var book = await _unitOfWork.BookRepository.GetById(bookToReturn.BookId);
+            if (book is null)
+                return null;
+
+            book.IsAvailable = true;
+            bookToReturn.ReturnDate = DateTime.Now;
+
+            await _unitOfWork.BookRepository.Update(book);
+            await _unitOfWork.BorrowTransactionRepository.Update(bookToReturn);
+            await _unitOfWork.CompleteAsync();
+
+            return await _unitOfWork.BorrowTransactionRepository.GetAll();
+        }
+    }
+}
