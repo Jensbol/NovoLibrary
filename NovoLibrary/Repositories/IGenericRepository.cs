c3699190864ef7678cfaad6ceac6b0b47cb6b7b0
Jensbol <72439442+Jensbol@users.noreply.github.com>
2023-06-05 21:13:41 +0200

    feat: Add Generic Repository to Add, Update, Delete, GetAll, and GetById for TEnity

---
+++ NovoLibrary/Repositories/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NovoLibrary.Data;
+
+namespace NovoLibrary.Core.Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        Task<List<TEntity>> GetAll();
+        Task<TEntity>? GetById(int id);
+        Task<bool> Add(TEntity entity);
+        Task<bool> Update(TEntity entity);
+        Task<bool> Delete(TEntity entity);
+
+    }
+}
