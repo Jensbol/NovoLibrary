9e07a195176aed04776acc3a041c7db89611a8dd
Jensbol <mjj.johansen@outlook.com>
2023-06-05 19:53:32 +0200

    feat: Add initial Controllers for Book, Member, and BorrowTransaction
    feat: Add initia Controllers for Book, Member, BorrowTransaction

---
+++ NovoLibrary/Controllers/BorrowTransactionController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NovoLibrary.Models;
+using NovoLibrary.Services.BookService;
+using NovoLibrary.Services.BorrowService;
+
+namespace NovoLibrary.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BorrowTransactionController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<ActionResult<List<Book>>> GetBooksByMember(int memberId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<BorrowTransaction>>> BorrowBook(BorrowTransaction borrowTransaction)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{bookId}")]
+        public async Task<ActionResult<List<BorrowTransaction>>> ReturnBook(int bookId)
+        {
+            throw new NotImplementedException();
+        }
+       
+
+    }
+}
---
+++ NovoLibrary/Controllers/MemberController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NovoLibrary.Data;
+using NovoLibrary.Models;
+using NovoLibrary.Services.BookService;
+using NovoLibrary.Services.MemberService;
+
+namespace NovoLibrary.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MemberController : ControllerBase
+    {
+        
+
+        [HttpGet]
+        public async Task<ActionResult<List<Member>>> GetAllMembers()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Member>> GetMemberById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Member>>> AddMember(Member memberRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<List<Member>>> UpdateMember(int id, Member memberRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Member>>> DeleteBook(int id)
+        {
+            throw new NotImplementedException();
+
+        }
+}
