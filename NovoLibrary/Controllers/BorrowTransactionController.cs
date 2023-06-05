f594fe5cc78c4162f8b16343967e58fdb157b60a
Jensbol <72439442+Jensbol@users.noreply.github.com>
2023-06-05 20:36:32 +0200

    feat: Add Controllers for Book, Member, and BorrowTransaction

---
+++ NovoLibrary/Controllers/BorrowTransactionController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NovoLibrary.Models;
+
+
+namespace NovoLibrary.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BorrowTransactionController : ControllerBase
+    {
+        
+
+        [HttpGet]
+        public async Task<ActionResult<List<Book>>> GetBooksByMember(int memberId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<BorrowTransaction>>> BorrowBook(BorrowTransaction borrowTransaction)
+        {
+            throw new NotImplementedException();
+
+
+        }
+
+        [HttpPut("{bookId}")]
+        public async Task<ActionResult<List<BorrowTransaction>>> ReturnBook(int bookId)
+        {
+            throw new NotImplementedException();
+        }
+       
+
+    }
+}
---
+++ NovoLibrary/Controllers/MemberController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NovoLibrary.Models;
+
+
+namespace NovoLibrary.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MemberController : ControllerBase
+    {
+        
+
+        [HttpGet]
+        public async Task<ActionResult<List<Member>>> GetAllMembers()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Member>> GetMemberById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Member>>> AddMember(Member memberRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<List<Member>>> UpdateMember(int id, Member memberRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Member>>> DeleteBook(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
