994ddbba44e71387e7d548c37c79ac9bd736d464
Jensbol <mjj.johansen@outlook.com>
2023-06-05 21:31:11 +0200

    feat: Update BookController and connect to BookService.

--- NovoLibrary/Controllers/BookController.cs
+++ NovoLibrary/Controllers/BookController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NovoLibrary.Data;
 using NovoLibrary.Models;
+using NovoLibrary.Services.BookService;
 
 namespace NovoLibrary.Controllers
 {
@@ -8,35 +10,55 @@ namespace NovoLibrary.Controllers
     [ApiController]
     public class BookController : ControllerBase
     {
+        private readonly IBookService _bookService;
+
+        public BookController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
 
         [HttpGet]
         public async Task<ActionResult<List<Book>>> GetAllBooks()
         {
-            throw new NotImplementedException();
+            var books = await _bookService.GetAllBooks();
+            return Ok(books);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Book>> GetBookById(int id)
         {
-            throw new NotImplementedException();
+            var book = await _bookService.GetBookById(id);
+            if (book is null)
+                return NotFound();
+
+            return Ok(book);
         }
 
         [HttpPost]
         public async Task<ActionResult<List<Book>>> AddBook(Book bookRequest)
         {
-            throw new NotImplementedException();
+            var book = await _bookService.AddBook(bookRequest);
+            return Ok(book);
         }
 
         [HttpPut("{id}")]
         public async Task<ActionResult<List<Book>>> UpdateBook(int id, Book book)
         {
-            throw new NotImplementedException();
+            var books = await _bookService.UpdateBook(id, book);
+            if (books is null)
+                return NotFound();
+
+            return Ok(books);
         }
 
         [HttpDelete("{id}")]
         public async Task<ActionResult<List<Book>>> DeleteBook(int id)
         {
-            throw new NotImplementedException();
+            var books = await _bookService.DeleteBook(id);
+            if (books is null)
+                return NotFound();
+
+            return Ok(books);
         }
 
     }
