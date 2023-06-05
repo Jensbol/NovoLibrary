5d7fb69d869da96970d588160f30efeaa60b4bb8
Jensbol <mjj.johansen@outlook.com>
2023-06-05 21:28:46 +0200

    feat: Add Book service for GetAllBooks, GetBookById, AddBook, DeleteBook, and UpdateBook.

---
+++ NovoLibrary/Services/BookService/IBookService.cs
@@ -0,0 +1,14 @@
+﻿using NovoLibrary.Models;
+
+namespace NovoLibrary.Services.BookService
+{
+    public interface IBookService
+    {
+        Task<List<Book>> GetAllBooks();
+        Task<Book>? GetBookById(int id);
+        Task<List<Book>> AddBook(Book book);
+        Task<List<Book>>? UpdateBook(int Id, Book book);
+       
+        Task<List<Book>>? DeleteBook(int id);
+    }
+}
