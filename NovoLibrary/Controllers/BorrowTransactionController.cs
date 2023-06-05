8e4705ad9d49e04756fc69d422f82a2bb29e1a4b
Jensbol <mjj.johansen@outlook.com>
2023-06-05 19:53:32 +0200

    feat: Add initial Controllers for Book, Member, and BorrowTransaction

---
+++ NovoLibrary/Controllers/BorrowTransactionController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NovoLibrary.Models;
+using NovoLibrary.Services.BookService;
+using NovoLibrary.Services.BorrowService;
+
+namespace NovoLibrary.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BorrowTransactionController : ControllerBase
+    {
+        private readonly IBorrowService _borrowService;
+
+        public BorrowTransactionController(IBorrowService borrowService)
+        {
+            _borrowService = borrowService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Book>>> GetBooksByMember(int memberId)
+        {
+            var borrow = await _borrowService.GetBooksByMember(memberId);
+            return Ok(borrow);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<BorrowTransaction>>> BorrowBook(BorrowTransaction borrowTransaction)
+        {
+            try
+            {
+                var borrow = await _borrowService.BorrowBook(borrowTransaction);
+                return Ok(borrow);
+            } catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+            
+            
+        }
+
+        [HttpPut("{bookId}")]
+        public async Task<ActionResult<List<BorrowTransaction>>> ReturnBook(int bookId)
+        {
+            var borrow = await _borrowService.ReturnBook(bookId);
+            return Ok(borrow);
+        }
+       
+
+    }
+}
---
+++ NovoLibrary/Controllers/MemberController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NovoLibrary.Data;
+using NovoLibrary.Models;
+using NovoLibrary.Services.BookService;
+using NovoLibrary.Services.MemberService;
+
+namespace NovoLibrary.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MemberController : ControllerBase
+    {
+        private readonly IMemberService _memberService;
+
+        public MemberController(IMemberService memberService)
+        {
+            _memberService = memberService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Member>>> GetAllMembers()
+        {
+            var members = await _memberService.GetAllMembers();
+            return Ok(members);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Member>> GetMemberById(int id)
+        {
+            var member = await _memberService.GetMemberById(id);
+            if (member is null)
+                return NotFound("The Member you were looking for was not found");
+
+            return Ok(member);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Member>>> AddMember(Member memberRequest)
+        {
+            var member = await _memberService.AddMember(memberRequest);
+            return Ok(member);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<List<Member>>> UpdateMember(int id, Member memberRequest)
+        {
+            var member = await _memberService.UpdateMember(id, memberRequest);
+            if (member is null)
+                return NotFound("The Member you are trying to update can not be found.");
+
+            return Ok(member);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Member>>> DeleteBook(int id)
+        {
+            var member = await _memberService.DeleteMember(id);
+            if (member is null)
+                return NotFound("The Member you are trying to delete was not found.");
+
+            return Ok(member);
+        }
+
+    }
+}
