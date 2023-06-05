6fd73e4a647409b55561087815944f80503d2166
Jensbol <mjj.johansen@outlook.com>
2023-06-05 22:04:23 +0200

    feat: Add MemberService for GetAllMembers, GetMemberById, AddMember, UpdateMember, DeleteMember

---
+++ NovoLibrary/Services/MemberService/IMemberService.cs
@@ -0,0 +1,14 @@
+﻿using NovoLibrary.Models;
+
+namespace NovoLibrary.Services.MemberService
+{
+    public interface IMemberService
+    {
+        Task<List<Member>> GetAllMembers();
+        Task<Member>? GetMemberById(int id);
+        Task<List<Member>> AddMember(Member member);
+        Task<List<Member>>? UpdateMember(int Id, Member request);
+
+        Task<List<Member>>? DeleteMember(int id);
+    }
+}
