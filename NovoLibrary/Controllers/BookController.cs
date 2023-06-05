430bba5142ed5fd795d386043028dda69eb343d0
Jensbol <mjj.johansen@outlook.com>
2023-06-05 20:28:48 +0200

    feat: Add Controllers for Book, Member, and BorrowTransaction

---
+++ NovoLibrary/Controllers/BookController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NovoLibrary.Models;
+
+namespace NovoLibrary.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<ActionResult<List<Book>>> GetAllBooks()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBookById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Book>>> AddBook(Book bookRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<List<Book>>> UpdateBook(int id, Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Book>>> DeleteBook(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
