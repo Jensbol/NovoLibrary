8e4f54d45a4945635f30dbbc1984ae35179b972f
Jensbol <mjj.johansen@outlook.com>
2023-06-05 20:40:52 +0200

    feat: Add DbContext and EF core

---
+++ NovoLibrary/Data/LibraryContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NovoLibrary.Models;
+
+namespace NovoLibrary.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Member> Members { get; set; }
+        public DbSet<BorrowTransaction> BorrowTransactions { get; set; }
+
+    }
+}
